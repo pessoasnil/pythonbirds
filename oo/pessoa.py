23ab35ab78c5e4924220180e279ed97b1cb85e3c
pessoasnil <Metroide171190y@>
2021-06-08 00:35:15 -0300

    Criados os atributos de Instãncia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None,idade=30):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Lins')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Pessoa'
+    print(p.nome)
+    print(p.idade)
+
 
