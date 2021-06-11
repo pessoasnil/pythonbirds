794df2e9952dec826034a79180c737a7849aae66
pessoasnil <Metroide171190y@>
2021-06-10 23:47:05 -0300

    Outro Teste

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -7,6 +7,10 @@ class CarroTestCase (TestCase):
         motor=Motor()
         self.assertEqual(0,motor.velocidade)
 
+    def teste_acelerar(self):
+        motor=Motor()
+        motor.acelerar()
+        self.assertEqual(1,motor.velocidade)
 
 
 
